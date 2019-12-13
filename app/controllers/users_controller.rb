04eb7800277be27caa6e07488cef766aa47811d3
sherrynganguyen <nganguyen.sfu@gmail.com>
2019-12-13 07:20:44 +0000

    add email validation for signup & css error msg

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,14 @@ class UsersController < ApplicationController
   def new
   end
   def create
+
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
       redirect_to '/'
     else
-      redirect_to '/signup'
+      @msg = user.errors.messages[:email][0]
+      redirect_to :back, notice: "Email #{@msg}"
     end
   end
   private
