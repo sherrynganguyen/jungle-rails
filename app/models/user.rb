1a544fbbaf724bd940305e9088cc02f4f36512e8
sherrynganguyen <nganguyen.sfu@gmail.com>
2019-12-17 03:30:47 +0000

    firstnam & lastname required

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :email, uniqueness: true
   validates :password, length: {minimum: 6, maximum: 14}
+  validates :firstname, presence: true
+  validates :lastname, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe User, type: :model do
     :firstname => "Pikachu",
     :lastname => "Pokemon",
     :email => "test@test.com",
-    :password => "123",
-    :password_confirmation => "123"
+    :password => "1234567",
+    :password_confirmation => "1234567"
     )
   }
   
@@ -14,6 +14,22 @@ RSpec.describe User, type: :model do
     it "should have same password and password confirmation" do
       expect(subject.password).to eq(subject.password_confirmation)
     end
+    it 'validates uniqueness of email' do
+      @user_with_same_email = subject.dup
+      @user_with_same_email.save
+      expect(subject.email).to eq(@user_with_same_email.email)
+    end
+    it "is not valid without a first name" do
+      subject.firstname = ""
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).to include("Firstname can't be blank")
+    end
+    it "is not valid without a last name" do
+      subject.lastname = nil
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).to include("Lastname can't be blank")
+    end
+
   end
 
   describe '.authenticate_with_credentials' do
