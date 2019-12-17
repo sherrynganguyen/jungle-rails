c1b65e24194e665c7a108f5ef641e5c38e2ae6d3
sherrynganguyen <nganguyen.sfu@gmail.com>
2019-12-17 03:46:08 +0000

    test for length of password, presence of password & pass confirm

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :email, uniqueness: true, presence: true
   validates :password, length: {minimum: 6, maximum: 14}
+  validates :password_confirmation, length: {minimum: 6, maximum: 14}
   validates :firstname, presence: true
   validates :lastname, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe User, type: :model do
     :firstname => "Pikachu",
     :lastname => "Pokemon",
     :email => "test@test.com",
-    :password => nil,
-    :password_confirmation => nil
+    :password => "1234567",
+    :password_confirmation => "1234567"
     )
   }
   
@@ -34,10 +34,25 @@ RSpec.describe User, type: :model do
       expect(subject).to_not be_valid
       expect(subject.errors.full_messages).to include("Email can't be blank")
     end
-    it "is not valid without a short password" do
+    it "is not valid without password" do
+      subject.password = nil
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).not_to be_empty
+    end
+    it "is not valid without password confirmation" do
+      subject.password_confirmation = nil
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).not_to be_empty
+    end
+    it "is not valid without a password less than 6 characters" do
       subject.password = "123"
       expect(subject).to_not be_valid
       expect(subject.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+    end
+    it "is not valid without a password more than 14 characters" do
+      subject.password = "12346456423423424234"
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).to include("Password is too long (maximum is 14 characters)")
     end
   end
 
