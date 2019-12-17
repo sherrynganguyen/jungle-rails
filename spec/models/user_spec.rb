a96f62e5ce401f93b3dea80a634c6c47d43a5346
sherrynganguyen <nganguyen.sfu@gmail.com>
2019-12-17 02:54:02 +0000

    initialize user-spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { described_class.new(
+    :firstname => "Pikachu",
+    :lastname => "Pokemon",
+    :email => "test@test.com",
+    :password => "123",
+    :password_confirmation => "123"
+    )
+  }
+  
+  describe 'Validations' do
+    it "should have same password and password confirmation" do
+      expect(subject.password).to be(subject.password_confirmation)
+    end
+  end
+
+  describe '.authenticate_with_credentials' do
+  end
+
 end
