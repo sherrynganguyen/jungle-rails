aeab4a3c43d325c5b3e9e43b2f3c696e1ef695c3
sherrynganguyen <nganguyen.sfu@gmail.com>
2019-12-18 00:44:05 +0000

    fix the header navigation

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -12,19 +12,21 @@ RSpec.feature "ProductDetails", type: :feature, js: true do
       )
     end
   end
-  # product = Product.find(product.id)
 
   scenario "Naviagate from Home Page to Product Details page" do
     visit root_path
-    page.first('article header').click_on
+    page.first('article header').click
     find('.product-detail')
-    save_screenshot ("product_details.png")
+    puts page.html
+    sleep(0.1) and save_screenshot ("product_details.png") #add 0.1ms sleep time in order to have photo loaded
     expect(page).to have_selector '.product-detail'
+    save_screenshot ("test.png")
   end
   scenario "Naviagate from Home Page to Product Details page" do
     visit root_path
     find_link("Details »", match: :first).click
     find('.product-detail')
+    puts page.html
     save_screenshot ("product_details1.png")
     expect(page).to have_selector '.product-detail'
   end
